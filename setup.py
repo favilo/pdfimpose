95418ee7889d98a5cb0fd5433a06d33fba73f703
Louis <spalax@gresille.org>
2015-06-05 01:40:18 +0200

    Updated date of copyright

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2014 Louis Paternault
+# Copyright 2014-2015 Louis Paternault
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
