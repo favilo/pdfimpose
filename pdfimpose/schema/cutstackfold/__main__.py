d1309eb713c06f3be01019b2fc9e65638b89a6b0
Louis <spalax@gresille.org>
2021-07-07 13:54:43 +0200

    Proper switch to GNU AGPLv3 licence (to comply with pymupdf licence)
    Closes #29

--- pdfimpose/schema/cutstackfold/__main__.py
+++ pdfimpose/schema/cutstackfold/__main__.py
@@ -1,17 +1,19 @@
 # Copyright 2011-2021 Louis Paternault
 #
+# This file is part of pdfimpose.
+#
 # Pdfimpose is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # Pdfimpose is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero Public License
-# along with pdfimpose.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with pdfimpose.  If not, see <https://www.gnu.org/licenses/>.
 
 """Parse arguments for the schema "cutstackfold"."""
 
